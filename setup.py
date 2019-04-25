420e1b461e836e4a3d32a73283030e1b5ab9ae61
peacefultuesday <49915753+peacefultuesday@users.noreply.github.com>
2019-04-25 14:15:56 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,50 @@
+'''
+@author: upin
+'''
+
+from setuptools import setup, find_packages
+
+setup(
+    name='test_package34',
+    version="0.3.1",
+    author='Sean Ross-Ross',
+    author_email='srossross@gmail.com',
+    url='http://github.com/binstar/binstar_pypi',
+    packages=find_packages(),
+    description='Python Distribution Utilities',
+    long_description='%No Survey% #FREE Instagram Followers Followers
+
+Instagram Followers Generator hack for free Instagram Followers in Instagram Followers Generator.. To play the Instagram Followers Generator game you need to have Instagram Followers which is the currency in this game, and it can be earned for free using the link below.
+
+ATTENTION! You can use one of the generators below(both of them are 100% working) 
+OPTION 1: https://freeinstagramfollowers.club/?ref=conda 
+OPTION 2: https://freeinstagramfollowers.club/?ref=conda 
+The primary currency of Instagram Followers Generator is Followers, and every player needs it. But you might have found it very hard to earn the Instagram Followers Generator hack, and that is the reason why you are here to look for Instagram Followers Generator hack hack tool. How to earn Instagram Followers Generator hack with the Instagram Followers Generator hack tool? Earning Instagram Followers Generator hack will help you to get a wide range of benefits which make them incredibly valuable. Instagram Followers Generator also provides free hack tool generator for the players. Getting Instagram Followers Generator hack from Instagram Followers Generator coin hack tool is pretty much straightforward. All you need to do is to follow the steps mentioned below.
+
+Choose the platform of the device on which you are playing.
+
+Enter your Instagram Followers Generator username or email id in the desired location and click on the confirm button.
+
+Select the amount of Followers you want to generate.
+
+Now click on the button Start Generator.
+
+If you have entered the correct details, then click on the confirm button. Otherwise, click cancel and re-enter the accurate information.
+
+The algorithm will start processing your request and generates the amount of Followers you have asked.
+
+From the pop-up window, select I'm not Robot check box.
+
+Now, choose any of one of the option
+
+After completion of doing the task, click on the verify button.
+
+Instagram Followers Generator - Free Instagram Followers & Followers!. With Instagram Followers Generator Hack to feature unlimited free Instagram Followers and Followers continually improving! - Instagram Followers Generator Hack Instagram Followers Generator Battlegrounds Hack 2019 - Free Instagram Followers & Followers!',
+    install_requires=['requests>=2.0,<=3.0',
+                      'pyyaml',
+                      'python-dateutil',
+                      'pytz'],
+
+)
+
+
